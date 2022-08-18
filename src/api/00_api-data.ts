3c48cfd0142fb1ef3713715e8b7e7607cfc15035
EgorKirillov <80296253508@mail.ru>
2022-08-18 11:20:10 +0300

    add gh pages

--- src/api/00_api-data.ts
+++ src/api/00_api-data.ts
@@ -3,8 +3,8 @@ import axios from 'axios'
 export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
-    headers: {
-        'API-KEY': '4f6e1a9b-a442-4dac-9b05-ef297eadf4f8'
-    }
+    // headers: {
+    //     'API-KEY': '4f6e1a9b-a442-4dac-9b05-ef297eadf4f8'
+    // }
 })
 
