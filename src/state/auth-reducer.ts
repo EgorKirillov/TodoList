74588d17d913fa945e1e28ac3f51fd6b128df931
EgorKirillov <80296253508@mail.ru>
2022-09-10 21:20:26 +0300

    refactor: auth reducer complite

--- src/state/auth-reducer.ts
+++ src/state/auth-reducer.ts
@@ -1,4 +1,3 @@
-import {Dispatch} from 'redux'
 import {setAppStatusAC, setIsInitializedAC} from "./app-reducer";
 import {handleServerAppError, handleServerNetworkError} from "../utils/error-utils";
 import {authAPI} from '../api/00_auth-api';
@@ -92,49 +91,9 @@ const slice = createSlice({
   
 })
 
-
-export const {setIsLoggedInAC} = slice.actions
-
 export const authReducer = slice.reducer
-// export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-//     switch (action.type) {
-//         case 'login/SET-IS-LOGGED-IN':
-//             return {...state, isLoggedIn: action.value}
-//         default:
-//             return state
-//     }
-// }
-// actions
-// export const setIsLoggedInAC = (value: boolean) =>
-//     ({type: 'login/SET-IS-LOGGED-IN', value} as const)
-
-// thunks
-
-
-export const _initializeAppTC = () => (dispatch: Dispatch) => {
-  
-  authAPI.me().then(res => {
-    //dispatch(setAppStatusAC("loading"))
-    if (res.data.resultCode === 0) {
-      dispatch(setIsLoggedInAC({isLoggedIn: true}));
-      
-    } else {
-    
-    }
-  }).catch((err) => {
-    console.log(err)
-  }).finally(() => {
-    dispatch(setIsInitializedAC({isInitialized: true}));
-    //dispatch(setAppStatusAC("idle"))
-    
-  })
-  
-}
-
 
 // types
-//type ActionsType = ReturnType<typeof setIsLoggedInAC>
-
 export type LoginDataType = {
   email: string
   password: string
